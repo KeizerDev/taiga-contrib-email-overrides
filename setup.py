f4bcaf4445c7bd0304a98b6672a16218abd56dbe
Robert-Jan Keizer <robertjankeizer@gmail.com>
2016-10-05 16:01:45 +0200

    Init commit

---
+++ setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import versiontools_support
+from setuptools import setup, find_packages
+import os
+
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+TEMPLATES = [
+    {
+        "DIRS": [
+            os.path.join(BASE_DIR, "templates/emails/"),
+        ],
+    }
+]
+
+
+setup(
+    name = 'taiga-email-overrides',
+    version = ":versiontools:taiga_contrib_slack:",
+    description = "The Taiga plugin for custom templates",
+    long_description = "",
+    keywords = 'taiga, Email, integration',
+    author = 'Studiofonkel',
+    author_email = 'info@studiofonkel.nl',
+    url = 'https://github.com/taigaio/taiga-contrib-slack',
+    license = '',
+    include_package_data = True,
+    packages = find_packages(),
+    install_requires=[],
+    setup_requires = [
+        'versiontools >= 1.9',
+    ],
+    classifiers = [
+        "Programming Language :: Python",
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+)
